fca843a064a647190bca16aefbfb0b1bfed72afd
Bruno Farias <bruno.farias@g.globo>
2021-10-15 18:44:19 -0300

    teste

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",
+    name="packaging_tutorial",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
